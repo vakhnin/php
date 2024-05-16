117994c3371f6c1ddc88e7d440b50a744ab04f47
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-05-16 13:33:58 +0300

    lesson01

---
+++ lesson01/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+</head>
+<body>    
+    <h1><?=$h1_text?></h1>
+
+    <?=$year?>
+</body>
+</html>
