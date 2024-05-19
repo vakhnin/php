43f022afae9f8805c037cea00d081630610ff907
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-05-19 12:46:54 +0300

    Задача 2

--- lesson02/lesson02.php
+++ lesson02/lesson02.php
@@ -28,7 +28,23 @@ echo "<br>";
 $a = rand(0, 15);
 
 function print_from_parameter_to_15($a) {
-    echo $a . "<br>";
+    switch ($a) {
+        case 1: echo "1<br>"; break;
+        case 2: echo "2<br>"; break;
+        case 3: echo "3<br>"; break;
+        case 4: echo "4<br>"; break;
+        case 5: echo "5<br>"; break;
+        case 6: echo "6<br>"; break;
+        case 7: echo "7<br>"; break;
+        case 8: echo "8<br>"; break;
+        case 9: echo "9<br>"; break;
+        case 10: echo "10<br>"; break;
+        case 11: echo "11<br>"; break;
+        case 12: echo "12<br>"; break;
+        case 13: echo "13<br>"; break;
+        case 14: echo "14<br>"; break;
+        case 15: echo "15<br>"; break;
+    }
     if ($a < 15) {
         print_from_parameter_to_15($a + 1);
     }
