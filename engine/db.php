92a12ad8c4fb9d0ec8a36598090ecdab8b09f0a8
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-06-15 10:09:37 +0300

    Сделана галерея маленьких изображений

--- engine/db.php
+++ engine/db.php
@@ -12,6 +12,16 @@ function getOneRow($sql) {
     return mysqli_fetch_assoc($result);
 }
 
+function getRows($sql)  {
+    $result = @mysqli_query(getDB(), $sql) or die(mysqli_error(getDB()));
+    $array_result = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array_result[] = $row;
+    }
+
+    return $array_result;
+}
+
 function executeSQL($sql) {
     @mysqli_query(getDB(), $sql) or die(mysqli_error(getDB()));
     return mysqli_affected_rows(getDB());
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,3 +1,13 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+define('SMALL_IMG_HREF', '/img/small/');
+define('BIG_IMG_HREF', '/img/big/');
+
+$img_array = getRows("SELECT * FROM `images`;");
+?>
 <h1>Галерея</h1>
 
-
+<?php foreach ($img_array as $img): ?>
+    <a href="?page=gallery&image=<?=$img["id"]?>"><img src="<?=SMALL_IMG_HREF?><?=$img["image"]?>" alt=""></a>
+<?php endforeach ?>
